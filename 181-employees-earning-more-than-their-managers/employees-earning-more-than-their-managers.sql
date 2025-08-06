60ce087c90166bd58c9dc27b40b80799caf67783
Nithishree Maheshwaran <177098799+nithishreemd@users.noreply.github.com>
2025-08-06 11:45:07 +0530

    Time: 404 ms (33.68%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,9 +1,4 @@
-select 
-    e.name AS 'Employee'
-from
-    Employee e
-    left join
-    Employee m
-    on e.managerId = m.id
-where
-    e.salary > m.salary;
+Select e.name as Employee from 
+Employee e join Employee m
+on e.managerId = m.id
+where e.salary>m.salary;
