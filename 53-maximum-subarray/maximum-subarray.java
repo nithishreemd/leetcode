cddf364e9ed40d4b7e87b650977c4efb4d8afee5
Nithishree Maheshwaran <177098799+nithishreemd@users.noreply.github.com>
2025-09-10 14:56:49 +0530

    Time: 1 ms (99.49%) | Memory: 57.2 MB (31.54%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,34 +1,23 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        return divideAndConquer(nums, 0, nums.length - 1);
-    }
-
-    private int divideAndConquer(int[] nums, int left, int right) {
-        if (left == right) return nums[left];
-
-        int mid = (left + right) / 2;
-
-        int leftMax = divideAndConquer(nums, left, mid);
-        int rightMax = divideAndConquer(nums, mid + 1, right);
-        int crossMax = maxCrossingSum(nums, left, mid, right);
-
-        return Math.max(Math.max(leftMax, rightMax), crossMax);
-    }
-
-    private int maxCrossingSum(int[] nums, int left, int mid, int right) {
-        int leftSum = Integer.MIN_VALUE, sum = 0;
-        for (int i = mid; i >= left; i--) {
-            sum += nums[i];
-            leftSum = Math.max(leftSum, sum);
+        int sum=0;
+        int max =Integer.MIN_VALUE;
+        for(int i =0;i<nums.length;i++)
+        {
+            max = Math.max(max,nums[i]);
         }
-
-        int rightSum = Integer.MIN_VALUE;
-        sum = 0;
-        for (int i = mid + 1; i <= right; i++) {
-            sum += nums[i];
-            rightSum = Math.max(rightSum, sum);
+        for(int i =0;i<nums.length;i++)
+        {
+            if(nums[i]+sum<0)
+            {
+                sum=0;
+                continue;
+            }
+            else
+            { sum+=nums[i];
+                max=Math.max(max,sum);
+            }
         }
-
-        return leftSum + rightSum;
+        return max;
     }
 }
